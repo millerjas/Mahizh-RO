3f4ccfa4677b40c269606aa6a3673b2bac8f451a
KALIL-devs <kalilrahman055@gmail.com>
2026-04-01 10:01:52 +0530

    fix card - kalil

--- src/app/services/[slug]/page.tsx
+++ src/app/services/[slug]/page.tsx
@@ -7,7 +7,6 @@ import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
 import ServicesCTA from "@/components/services/ServicesCTA";
 import ServiceDetailTemplate from "@/components/services/ServiceDetailTemplate";
-import PreviousWork from "@/components/services/PreviousWork";
 import ServiceTestimonials from "@/components/services/ServiceTestimonials";
 import OtherServices from "@/components/services/OtherServices";
 
@@ -74,7 +73,6 @@ export default async function ServiceDetailPage({
 
       <main style={{ paddingTop: "80px" }}>
         <ServiceDetailTemplate data={data} headerImage={card?.image} />
-        <PreviousWork />
         <ServiceTestimonials currentSlug={slug} />
         <OtherServices currentSlug={slug} />
         <ServicesCTA />
